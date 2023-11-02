2eb7d451e99ee8c07d00ca7d8eeb416794087a99
Rezarafi97 <rezarafi901@gmail.com>
2023-11-02 12:14:36 +0330

    fixing

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,103 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 
-import { RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import { ThemeProvider } from "@material-tailwind/react";
 import { store } from "./store";
 
-import { router } from "./routes";
 import "./index.css";
-
 import "react-toastify/dist/ReactToastify.css";
+
 import { Loading } from "./components/common";
+import App from "./App.jsx";
+
+import {
+  Error,
+  SignUp,
+  Login,
+  Wishlist,
+  Cart,
+  CheckOut,
+  Account,
+  About,
+  Contact,
+  ProductDetail,
+  Home,
+  AllProducts,
+  Catagory,
+} from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/wishlist",
+        element: <Wishlist />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/checkout",
+        element: <CheckOut />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/account",
+        element: <Account />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/productdetail/:productID",
+        element: <ProductDetail />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/allproducts",
+        element: <AllProducts />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/allproducts/:catagory",
+        element: <Catagory />,
+        errorElement: <Error />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
